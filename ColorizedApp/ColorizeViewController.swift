507738f54344ad51efb328ae646229dd9684db6e
Dmitry Parkhomenko <oku_74@mail.ru>
2023-12-09 15:52:07 +0500

    1. Создал в проект без передачи данных между ViewControllers. 2. Проработал ошибки прошлого задания

---
+++ ColorizedApp/ColorizeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ColorizeViewController.swift
+//  ColorizedApp
+//
+//  Created by Dmitry Parhomenko on 08.12.2023.
+//
+
+import UIKit
+
+class ColorizeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
